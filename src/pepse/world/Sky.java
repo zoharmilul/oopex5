a8faa7ee97cd3d029a9849005f4871979b60619b
Home <rachel.kreiter@mail.huji.ac.il>
2022-01-06 23:39:18 +0200

    jubhgj

--- src/pepse/world/Sky.java
+++ src/pepse/world/Sky.java
@@ -8,30 +8,32 @@ import danogl.util.Vector2;
 
 import java.awt.*;
 
+/**
+ * Represents the sky.
+ */
 public class Sky {
-
 
     //static consts
     private static final Color BASIC_SKY_COLOR = Color.decode("#80C6E5");
 
 
     /**
-     * create new sky
-     * @param gameObjects
-     * @param windowDimensions
-     * @param skyLayer
+     * This function creates a light blue rectangle which is always at the back of the window.
+     *
+     * @param gameObjects      game object collection instance
+     * @param windowDimensions dimensions of the game window
+     * @param skyLayer         layer to create the sky in
      * @return the sky (wow)
      */
     public static GameObject create(GameObjectCollection gameObjects,
-                                    Vector2 windowDimensions, int skyLayer){
+                                    Vector2 windowDimensions, int skyLayer) {
         GameObject sky = new GameObject(
                 Vector2.ZERO, windowDimensions,
                 new RectangleRenderable(BASIC_SKY_COLOR));
         sky.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
-        gameObjects.addGameObject(sky,skyLayer);
+        gameObjects.addGameObject(sky, skyLayer);
         sky.setTag("sky"); //for debugging
         return sky;
     }
-
 
 }
