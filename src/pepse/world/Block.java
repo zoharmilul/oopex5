1f9755a1bf3c2a6cdc32ffc748369f4e88bad52c
Home <rachel.kreiter@mail.huji.ac.il>
2022-01-06 23:36:42 +0200

    jubhgj

--- src/pepse/world/Block.java
+++ src/pepse/world/Block.java
@@ -5,10 +5,18 @@ import danogl.components.GameObjectPhysics;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
+/**
+ * Block Represents a single block (larger objects can be created from blocks).
+ */
 public class Block extends GameObject {
 
         public static final int SIZE = 30;
 
+    /**
+     * constructor
+     * @param topLeftCorner top left corner
+     * @param renderable renderable
+     */
         public Block(Vector2 topLeftCorner, Renderable renderable) {
             super(topLeftCorner, Vector2.ONES.mult(SIZE), renderable);
             physics().preventIntersectionsFromDirection(Vector2.ZERO);
