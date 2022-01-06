c3eb3d86e904793a8b344d0345a2f0ae35580cf0
Home <rachel.kreiter@mail.huji.ac.il>
2022-01-06 23:40:52 +0200

    jubhgj

--- src/pepse/world/daynight/SunHalo.java
+++ src/pepse/world/daynight/SunHalo.java
@@ -8,8 +8,18 @@ import danogl.util.Vector2;
 
 import java.awt.*;
 
+/**
+ * Represents the halo of sun.
+ */
 public class SunHalo {
-
+    /**
+     * This function creates a halo around a given object that represents the sun.
+     * @param gameObjects The collection of all participating game objects.
+     * @param layer The number of the layer to which the created halo should be added.
+     * @param sun  A game object representing the sun (it will be followed by the created game object).
+     * @param color The color of the halo.
+     * @return A new game object representing the sun's halo.
+     */
     public static GameObject create(
             GameObjectCollection gameObjects,
             int layer,
